cea221bcf1090dbc5c014b2a96d7cc1e380d565a
SuprajaSana <sanasupraja2727@gmail.com>
2022-10-30 08:58:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  
+  const [movies,setMovies]=useState([]) 
+
+  async function fetchDataHandler(){
+
+    const response=await fetch('https://swapi.dev/api/films/')
+     const data =await response.json()
+
+      const transformedData=data.results.map((movieData)=>{
+         return{
+          id:movieData.episode_id,
+          title:movieData.title,
+          openingText:movieData.opening_crawl,
+          releaseDate:movieData.release_date,
+         }         
+      })
+      setMovies(transformedData);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchDataHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies}/>
+      </section>
+    </React.Fragment>
   );
 }
 
 export default App;
+
